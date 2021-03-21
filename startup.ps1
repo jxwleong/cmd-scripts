82661ea056d253f0d9305e1b5f673d00f04b5f4e
jxwleong <jason.xie.wei.leong@gmail.com>
2021-03-21 13:43:00 +0800

    Added my simple startup powershell script to run ccleaner and microsoft defender

---
+++ startup.ps1
@@ -0,0 +1,16 @@
+﻿function Invoke-UdfGetLastOperationStatus(){
+    If ($? -eq $True){
+        return "PASS"
+    } Else {
+        return "FAIL"
+    }
+}
+
+Write-Output "$(Get-Date) Starting CCleaner..."
+CCleaner.exe /AUTO
+Write-Output "$(Get-Date) CCleaner launch status: $(Invoke-UdfGetLastOperationStatus)"
+
+# https://docs.microsoft.com/en-us/windows/security/threat-protection/microsoft-defender-antivirus/command-line-arguments-microsoft-defender-antivirus
+Write-Output "$(Get-Date) Starting Microsoft Defender with Quick Scan"
+MpCmdRun.exe -Scan -ScanType 1
+Write-Output "$(Get-Date) Microsoft Defender status: $(Invoke-UdfGetLastOperationStatus)"
