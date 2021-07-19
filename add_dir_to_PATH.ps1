5c0cf8112011f26bf77e5907162b9a71f9c5a0ed
jxwleong <jason.xie.wei.leong@gmail.com>
2021-07-19 20:12:35 +0800

    Test compilation of powershell script (ps1) => exe via ps2exe. * Some variable such as $PSCommandPath and command to get the current file path is not working on the exe version. * To compile:   ps2exe add_dir_to_PATH.ps1 add_dir_to_PATH.exe * Just realize that powershell have pypi equivalent which is powershell gallery. * PS2EXE: https://github.com/MScholtes/PS2EXE * Added sample script to convert powershell script to bat.

--- add_dir_to_PATH.ps1
+++ add_dir_to_PATH.ps1
@@ -13,8 +13,26 @@ function Log-Message
 
 $file = New-Object System.IO.FileInfo($PSCommandPath)
 $filename = -join($file.BaseName, $file.Extension)
-
-Log-Message "Current File Dir: $PSScriptRoot"
+$ScriptName = $MyInvocation.MyCommand.Name
+Write-Output "`nName of the script : $scriptName"
+$Currentlocation = Get-Location
+Log-Message "Current File Dir: $Currentlocation"
+Write-Host (Get-Item .).FullName
 Log-Message "File Name: $filename"
 Log-Message "Full File Path: $PSCommandPath"
+Log-Message "Current Working Directory: $pwd"
 
+
+# https://stackoverflow.com/a/53134449
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript")
+{ 
+   $ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition 
+}
+else
+{ 
+   $ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0]) 
+   if (!$ScriptPath){ $ScriptPath = "." } 
+}
+return $ScriptPath
+Write-Host $ScriptPath
+Start-Sleep -s 5
