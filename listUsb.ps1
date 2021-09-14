655b43780d336e9660b858a979f9b9419bca3e3d
jxwleong <jason.xie.wei.leong@gmail.com>
2021-09-14 19:47:27 +0800

    Added listUsb.ps1 to get connected USB device * Reference: https://winaero.com/how-to-find-and-list-connected-usb-devices-in-windows-10/#Find_and_List_Connected_USB_Devices_in_Windows_10

---
+++ listUsb.ps1
@@ -0,0 +1,8 @@
+# Reference: https://winaero.com/how-to-find-and-list-connected-usb-devices-in-windows-10/#Find_and_List_Connected_USB_Devices_in_Windows_10
+Get-PnpDevice -PresentOnly | Where-Object { $_.InstanceId -match '^USB' }
+
+echo "`nClass = USB"
+Get-PnpDevice -PresentOnly | Where-Object { $_.Class -match 'USB' }
+
+echo "`nFriendlyName = USB"
+Get-PnpDevice -PresentOnly | Where-Object { $_.Class -match 'USB' }
